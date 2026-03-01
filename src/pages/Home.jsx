9a0c76889a89a3952352a1915d592c4faf9eb180
Miranda Johnson <busannex@gmail.com>
2026-03-01 01:57:32 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import '@/components/styles/frontend.css';
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Link } from 'react-router-dom';
