bc3184b79ad69f18be07e09d6828983ac697987f
Miranda Johnson <busannex@gmail.com>
2026-03-31 13:06:32 +0000

    File changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
-import './globals.css'
 
 // Register PWA service worker
 if ('serviceWorker' in navigator) {
